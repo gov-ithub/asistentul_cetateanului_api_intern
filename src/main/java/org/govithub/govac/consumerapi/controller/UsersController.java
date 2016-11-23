a07d5188c100096d36a2e7e2dae1a43e7d7de65c
Cosmin <poterascosmin@yahoo.com>
2016-11-23 18:27:41 +0200

    Bug fix: User data is checked before updating the database

--- src/main/java/org/govithub/govac/consumerapi/controller/UsersController.java
+++ src/main/java/org/govithub/govac/consumerapi/controller/UsersController.java
@@ -20,22 +20,27 @@ public class UsersController {
 	public UsersController(UserRepository userRepository) {
 		this.usersRepo = userRepository;
 	}
-	
-	
+
 	@RequestMapping(method = RequestMethod.POST)
-	public User updateUser(@PathVariable(value="id") long userId, @RequestBody User user) throws GovacException {
+	public User updateUser(@PathVariable(value = "id") long userId, @RequestBody User user) throws GovacException {
 		User existingUser = usersRepo.findOne(userId);
 		if (existingUser == null) {
 			throw new GovacException("Invalid user.");
 		}
-		
-		existingUser.username = user.username;
-		existingUser.firstName = user.firstName;
-		existingUser.lastName = user.lastName;
-		existingUser.cnp = user.cnp;
-		existingUser.email = user.email;
-		existingUser.phone = user.phone;
-		
+
+		if (user.username != null && !user.username.isEmpty())
+			existingUser.username = user.username;
+		if (user.firstName != null && !user.firstName.isEmpty())
+			existingUser.firstName = user.firstName;
+		if (user.lastName != null && !user.lastName.isEmpty())
+			existingUser.lastName = user.lastName;
+		if (user.cnp != null && !user.cnp.isEmpty())
+			existingUser.cnp = user.cnp;
+		if (user.email != null && !user.email.isEmpty())
+			existingUser.email = user.email;
+		if (user.phone != null && !user.phone.isEmpty())
+			existingUser.phone = user.phone;
+
 		existingUser = usersRepo.save(existingUser);
 		return existingUser;
 	}
